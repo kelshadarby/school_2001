5e0562a1476d54df8c320b2e08582c9c1b52d007
Kelsha_Darby <ohanaipo@gmail.com>
2020-01-29 13:25:57 -0700

    Add add_student name method and end_time method

--- lib/school.rb
+++ lib/school.rb
@@ -8,23 +8,30 @@ class School
     @student_names = []
   end
 
+  def add_student_name(name)
+    @student_names << name
+  end
+
+  def end_time
+    end_time = @hours_in_school_day.to_i + @start_time.to_i
+    "#{end_time.to_s}:00"
+  end
+
 end
 
 
 
 
 
-# pry(main)> require './lib/school'
-# #=> true
-#
 # pry(main)> school = School.new('9:00', 7)
 # #=> #<School:0x007fcaf4957830 @hours_in_school_day=7, @start_time="9:00", @student_names=[]>
 #
-# pry(main)> school.start_time
-# #=> "9:00"
-#
-# pry(main)> school.hours_in_school_day
-# #=> 7
+# pry(main)> school.add_student_name('Aurora')
+# pry(main)> school.add_student_name('tim')
+# pry(main)> school.add_student_name('megan')
 #
 # pry(main)> school.student_names
-# #=> []
+# #=> ["Aurora", "tim", "megan"]
+#
+# pry(main)> school.end_time
+# #=> "16:00"
